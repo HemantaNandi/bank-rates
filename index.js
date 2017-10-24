9cf784275bb6e27f9954ed666d99cdc2b6015d29
Hemanta Nandi <hemantanandi2@gmail.com>
2017-10-25 00:05:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const APIAI_TOKEN = '94b99026461a40308de22bd4463600ec';
+const APIAI_SESSION_ID = 'tabb';
+
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/views')); // html
+app.use(express.static(__dirname + '/public')); // js, css, images
+
+const server = app.listen(process.env.PORT || 5000, () => {
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
+
+const io = require('socket.io')(server);
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+const apiai = require('apiai')('94b99026461a40308de22bd4463600ec');
+
+// Web UI
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
+
+io.on('connection', function(socket) {
+  socket.on('chat message', (text) => {
+    console.log('Message: ' + text);
+
+    // Get a reply from API.ai
+
+    let apiaiReq = apiai.textRequest(text, {
+      sessionId: 'tabby_cat'
+    });
+
+    apiaiReq.on('response', (response) => {
+      let aiText = response.result.fulfillment.speech;
+      console.log('Bot reply: ' + aiText);
+      socket.emit('bot reply', aiText);
+    });
+
+    apiaiReq.on('error', (error) => {
+      console.log(error);
+    });
+
+    apiaiReq.end();
+
+  });
+});
